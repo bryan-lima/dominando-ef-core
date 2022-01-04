da335e7bbc50d754be8936372e7be8612ad23f9a
Bryan <bryanlima200@outlook.com>
2022-01-04 19:02:38 -0300

    Adicionado classes Departamento e Funcionario

---
+++ DominandoEFCore/Domain/Funcionario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DominandoEFCore.Domain
+{
+    public class Funcionario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public int DepartamentoId { get; set; }
+
+        public Departamento Departamento { get; set; }
+    }
+}
