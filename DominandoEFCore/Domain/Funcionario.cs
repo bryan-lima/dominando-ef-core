021f737a8c61d407f6db9fde5dfdfd4ba7e805ea
Bryan <bryanlima200@outlook.com>
2022-01-08 15:38:56 -0300

    Adicionado propriedade Rg na classe Funcionario

--- DominandoEFCore/Domain/Funcionario.cs
+++ DominandoEFCore/Domain/Funcionario.cs
@@ -11,6 +11,7 @@ namespace DominandoEFCore.Domain
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Cpf { get; set; }
+        public string Rg { get; set; }
         public int DepartamentoId { get; set; }
 
         public Departamento Departamento { get; set; }
