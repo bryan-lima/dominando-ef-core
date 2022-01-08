12c86d0011b7e1da4580d24d32957524c684d106
Bryan <bryanlima200@outlook.com>
2022-01-08 20:30:59 -0300

    Adicionado terceiro exemplo de implementação de carregamento lento (LazyLoad), utilizando injeção de dependência do Action

--- DominandoEFCore/Domain/Departamento.cs
+++ DominandoEFCore/Domain/Departamento.cs
@@ -18,16 +18,34 @@ namespace DominandoEFCore.Domain
 
         }
 
-        private ILazyLoader _lazyLoader { get; set; }
-        private Departamento(ILazyLoader lazyLoader)
+        private Action<object, string> _lazyLoader { get; set; }
+        private Departamento(Action<object, string> lazyLoader)
         {
             _lazyLoader = lazyLoader;
         }
 
         private List<Funcionario> _funcionarios;
-        public List<Funcionario> Funcionarios {
-            get => _lazyLoader.Load(this, ref _funcionarios);
+        public List<Funcionario> Funcionarios
+        {
+            get
+            {
+                _lazyLoader?.Invoke(this, nameof(Funcionarios));
+
+                return _funcionarios;
+            }
             set => _funcionarios = value;
         }
+
+        //private ILazyLoader _lazyLoader { get; set; }
+        //private Departamento(ILazyLoader lazyLoader)
+        //{
+        //    _lazyLoader = lazyLoader;
+        //}
+
+        //private List<Funcionario> _funcionarios;
+        //public List<Funcionario> Funcionarios {
+        //    get => _lazyLoader.Load(this, ref _funcionarios);
+        //    set => _funcionarios = value;
+        //}
     }
 }
