5114d7a4d500de54c944d5bfe191d702031b9359
Bryan <bryanlima200@outlook.com>
2022-01-08 22:12:12 -0300

    Adicionado métodos FiltroGlobal e Setup na classe Program Neste momento, está sendo retornado todos os departamentos indepedente do status da propriedade Excluido

--- DominandoEFCore/Program.cs
+++ DominandoEFCore/Program.cs
@@ -14,7 +14,66 @@ namespace DominandoEFCore
     {
         static void Main(string[] args)
         {
+            FiltroGlobal();
+        }
 
+        static void FiltroGlobal()
+        {
+            using var db = new ApplicationContext();
+            Setup(db);
+
+            var departamentos = db.Departamentos.Where(p => p.Id > 0).ToList();
+
+            foreach (var departamento in departamentos)
+            {
+                Console.WriteLine($"Descrição: {departamento.Descricao} \t Excluído: {departamento.Excluido}");
+            }
+        }
+
+        static void Setup(ApplicationContext db)
+        {
+            if (db.Database.EnsureCreated())
+            {
+                db.Departamentos.AddRange(
+                    new Departamento
+                    {
+                        Ativo = true,
+                        Descricao = "Departamento 01",
+                        Funcionarios = new List<Funcionario>
+                        {
+                            new Funcionario
+                            {
+                                Nome = "Bryan Lima",
+                                Cpf = "12345678911",
+                                Rg = "113332220"
+                            }
+                        },
+                        Excluido = true
+                    },
+                    new Departamento
+                    {
+                        Ativo = true,
+                        Descricao = "Departamento 02",
+                        Funcionarios = new List<Funcionario>
+                        {
+                            new Funcionario
+                            {
+                                Nome = "Bruno Brito",
+                                Cpf = "88888888811",
+                                Rg = "3100062"
+                            },
+                            new Funcionario
+                            {
+                                Nome = "Eduardo Pires",
+                                Cpf = "77777777711",
+                                Rg = "1100062"
+                            }
+                        }
+                    });
+                
+                db.SaveChanges();
+                db.ChangeTracker.Clear();
+            }
         }
     }
 }
