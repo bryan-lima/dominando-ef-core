d23f92c2e9dc0f637d6477c687a3f2df14bda752
Bryan <bryanlima200@outlook.com>
2022-01-12 23:55:17 -0300

    Adicionado exemplo de como ignorar a configuração global de uso do SplitQuery (consultas que possuem relacionamento) para alguma consulta específica, obtendo assim o comportamento original

--- DominandoEFCore/Program.cs
+++ DominandoEFCore/Program.cs
@@ -211,6 +211,7 @@ namespace DominandoEFCore
             var departamentos = db.Departamentos.Include(departamento => departamento.Funcionarios)
                                                 .Where(departamento => departamento.Id < 3)
                                                 //.AsSplitQuery()
+                                                .AsSingleQuery()
                                                 .ToList();
 
             foreach (var departamento in departamentos)
