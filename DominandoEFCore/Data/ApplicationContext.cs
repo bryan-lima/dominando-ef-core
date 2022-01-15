3fde2d322eb0574791a5aa88ecd11ab249377db0
Bryan <bryanlima200@outlook.com>
2022-01-15 12:54:18 -0300

    Adicionado filtro no log, exibindo apenas os logs do tipo Information (INFO) no console

--- DominandoEFCore/Data/ApplicationContext.cs
+++ DominandoEFCore/Data/ApplicationContext.cs
@@ -19,7 +19,7 @@ namespace DominandoEFCore.Data
             const string strConnection = "Data Source=DESKTOP-B76722G\\SQLEXPRESS; Initial Catalog=DominandoEFCore; User ID=developer; Password=dev*10; Integrated Security=True; Persist Security Info=False; Pooling=False; MultipleActiveResultSets=False; Encrypt=False; Trusted_Connection=False";
 
             optionsBuilder.UseSqlServer(strConnection)
-                          .LogTo(Console.WriteLine);
+                          .LogTo(Console.WriteLine, LogLevel.Information);
         }
     }
 }
