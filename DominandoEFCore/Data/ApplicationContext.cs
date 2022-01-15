c7ca21cadca8973b63a9c68f75392a689e126807
Bryan <bryanlima200@outlook.com>
2022-01-15 15:22:22 -0300

    Iniciado desenvolvimento de exemplo de como habilitar a exibição dos dados enviados na consulta a base de dados Neste momento, ao executar a aplicação, no console ainda não é exibido os dados enviados

--- DominandoEFCore/Data/ApplicationContext.cs
+++ DominandoEFCore/Data/ApplicationContext.cs
@@ -23,13 +23,13 @@ namespace DominandoEFCore.Data
             const string strConnection = "Data Source=DESKTOP-B76722G\\SQLEXPRESS; Initial Catalog=DominandoEFCore; User ID=developer; Password=dev*10; Integrated Security=True; Persist Security Info=False; Pooling=False; MultipleActiveResultSets=False; Encrypt=False; Trusted_Connection=False";
 
             optionsBuilder.UseSqlServer(strConnection)
-                          //.LogTo(Console.WriteLine, LogLevel.Information)
+                          .LogTo(Console.WriteLine, LogLevel.Information);
                           //.LogTo(Console.WriteLine, 
                           //       new[] { CoreEventId.ContextInitialized, RelationalEventId.CommandExecuted }, 
                           //       LogLevel.Information,
                           //       DbContextLoggerOptions.LocalTime | DbContextLoggerOptions.SingleLine)
                           //.LogTo(_writer.WriteLine, LogLevel.Information)
-                          .EnableDetailedErrors();
+                          //.EnableDetailedErrors();
         }
 
         public override void Dispose()
