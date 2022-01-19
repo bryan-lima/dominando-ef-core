4f20d13d4766ceb8655fc8502a75c66cc506f938
Bryan <bryanlima200@outlook.com>
2022-01-18 23:08:56 -0300

    Adicionado classe ConversorCustomizado

---
+++ DominandoEFCore/Conversores/ConversorCustomizado.cs
@@ -0,0 +1,32 @@
+﻿using DominandoEFCore.Domain;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DominandoEFCore.Conversores
+{
+    public class ConversorCustomizado : ValueConverter<Status, string>
+    {
+        public ConversorCustomizado() : base(statusParaSalvarNoBD => ConverterParaBancoDeDados(statusParaSalvarNoBD),
+                                             statusConsultadoDoBD => ConverterParaAplicacao(statusConsultadoDoBD))
+        {
+
+        }
+
+        static string ConverterParaBancoDeDados(Status status)
+        {
+            return status.ToString()[0..1];
+        }
+
+        static Status ConverterParaAplicacao(string value)
+        {
+            Status status = Enum.GetValues<Status>()
+                                .FirstOrDefault(status => status.ToString()[0..1] == value);
+            
+            return status;
+        }
+    }
+}
