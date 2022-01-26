fb849857364b15d7ba32cf92438b37f720f9197d
Bryan <bryanlima200@outlook.com>
2022-01-25 21:54:48 -0300

    Adicionado classe Documento e configurado campo de apoio em DocumentoConfiguration

---
+++ DominandoEFCore/Configurations/DocumentoConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using DominandoEFCore.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DominandoEFCore.Configurations
+{
+    public class DocumentoConfiguration : IEntityTypeConfiguration<Documento>
+    {
+        public void Configure(EntityTypeBuilder<Documento> builder)
+        {
+            builder.Property(documento => documento.CPF)
+                   .HasField("_cpf");
+        }
+    }
+}
