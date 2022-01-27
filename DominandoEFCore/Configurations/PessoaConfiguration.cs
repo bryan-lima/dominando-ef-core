c5d0de41bfbcd5387ac656700f1ca34c85a4daa8
Bryan <bryanlima200@outlook.com>
2022-01-26 23:54:34 -0300

    Adicionado PessoaConfiguration e customizado coluna Discriminator (da tabela Pessoas - lembrando que é TPH) alterando nome para TipoPessoa, tipo de dado de nvarchar(max) para int e atribuindo um valor inteiro para cada tipo: Pessoa, Instrutor e Aluno

---
+++ DominandoEFCore/Configurations/PessoaConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using DominandoEFCore.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DominandoEFCore.Configurations
+{
+    public class PessoaConfiguration : IEntityTypeConfiguration<Pessoa>
+    {
+        public void Configure(EntityTypeBuilder<Pessoa> builder)
+        {
+            builder.ToTable("Pessoas")
+                   .HasDiscriminator<int>("TipoPessoa")
+                   .HasValue<Pessoa>(3)
+                   .HasValue<Instrutor>(6)
+                   .HasValue<Aluno>(99);
+        }
+    }
+}
