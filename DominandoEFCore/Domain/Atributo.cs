b2841635c4894e25eaf19b01a4fee9b4e5022d10
Bryan <bryanlima200@outlook.com>
2022-01-30 17:29:45 -0300

    Resolvido gap informado no commit anterior, usando Data Annotation InverseProperty Neste momento, ao executar a aplicação, o EF Core irá conseguir criar as tabelas com as devidas relações entre as entidades Aeroporto e Voo O atributo InverseProperty pode ser utilizada na classe principal ou na classe dependente, neste exemplo foi aplicada na classe principal

--- DominandoEFCore/Domain/Atributo.cs
+++ DominandoEFCore/Domain/Atributo.cs
@@ -27,7 +27,9 @@ namespace DominandoEFCore.Domain
         public int Id { get; set; }
         public string Nome { get; set; }
 
+        [InverseProperty("AeroportoPartida")]
         public ICollection<Voo> VoosDePartida { get; set; }
+        [InverseProperty("AeroportoChegada")]
         public ICollection<Voo> VoosDeChegada { get; set; }
     }
 
