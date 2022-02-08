93601e797683321f42347a41efdc3a70fc18d5c3
Bryan <bryanlima200@outlook.com>
2022-02-07 23:21:46 -0300

    Adicionado método Setup para popular as tabelas Departamentos e Funcionarios

--- DominandoEFCore/Program.cs
+++ DominandoEFCore/Program.cs
@@ -18,7 +18,29 @@ namespace DominandoEFCore
     {
         static void Main(string[] args)
         {
+            Setup();
+        }
 
+        static void Setup()
+        {
+            using ApplicationContext db = new ApplicationContext();
+            db.Database.EnsureDeleted();
+            db.Database.EnsureCreated();
+
+            db.Departamentos.Add(new Departamento 
+            {
+                Descricao = "Departamento Teste",
+                Ativo = true,
+                Funcionarios = Enumerable.Range(1, 100)
+                                         .Select(numero => new Funcionario
+                {
+                    Cpf = numero.ToString().PadLeft(11, '0'),
+                    Nome = $"Funcionário {numero}",
+                    Rg = numero.ToString()
+                }).ToList()
+            });
+
+            db.SaveChanges();
         }
     }
 }
